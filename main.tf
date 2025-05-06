585040d4cb140a3e1ff65ab704db4142123de9e4
Shreya <shreyanagar1591@gmailcom>
2025-05-06 21:09:38 +0530

    Add tests, requirements, and GitHub Actions workflow

--- main.tf
+++ main.tf
@@ -19,3 +19,12 @@ provider "aws" {
   region = "us-east-1"
 
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket120"
+    key            = "lambda/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
