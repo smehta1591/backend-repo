6cf356dbff54378565a319ce7601f03e5a307393
Shreya <shreyanagar1591@gmailcom>
2025-05-06 17:19:12 +0530

    Add tests, requirements, and GitHub Actions workflow

--- apigateway-lambda.tf
+++ apigateway-lambda.tf
@@ -68,17 +68,3 @@ resource "aws_apigatewayv2_stage" "default" {
 }
 
 
-resource "aws_dynamodb_table" "VisitCounter" {
-  name         = "VisitCounter"
-  billing_mode = "PAY_PER_REQUEST"  # or use provisioned settings if needed
-  hash_key     = "id"
-
-  attribute {
-    name = "id"
-    type = "S"  # 'S' for string, 'N' for number, etc.
-  }
-
-  tags = {
-    Name = "VisitCounter"
-  }
-}
